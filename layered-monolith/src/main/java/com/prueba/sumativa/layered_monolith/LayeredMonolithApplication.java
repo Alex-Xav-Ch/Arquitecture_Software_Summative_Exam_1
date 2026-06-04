2bc9afe3ebd8f010e40b4f0c2fdcddb9fe790010
Alex-Xav-Ch <alex.xavier.sisu26@gmail.com>
2026-06-04 11:01:09 -0500

    feat: add initial layered monolith structure

---
+++ layered-monolith/src/main/java/com/prueba/sumativa/layered_monolith/LayeredMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.sumativa.layered_monolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayeredMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LayeredMonolithApplication.class, args);
+	}
+
+}
