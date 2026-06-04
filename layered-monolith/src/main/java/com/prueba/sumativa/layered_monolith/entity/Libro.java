7b5133dd64023f7e795780b8ad4381d6e4826e34
Alex-Xav-Ch <alex.xavier.sisu26@gmail.com>
2026-06-04 12:55:37 -0500

    fix: resolve H2 table creation issue

--- layered-monolith/src/main/java/com/prueba/sumativa/layered_monolith/entity/Libro.java
+++ layered-monolith/src/main/java/com/prueba/sumativa/layered_monolith/entity/Libro.java
@@ -5,14 +5,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "libros")
 public class Libro {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     @Column(unique = true, nullable = false)
     private String isbn;
 
