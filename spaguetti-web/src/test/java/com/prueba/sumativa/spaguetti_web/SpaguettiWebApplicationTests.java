41962a17ac25c2232a67d4144dce1d166cac7c76
Alex-Xav-Ch <alex.xavier.sisu26@gmail.com>
2026-06-04 09:18:12 -0500

    feat: Add the initial project structure and h2 configuration

---
+++ spaguetti-web/src/test/java/com/prueba/sumativa/spaguetti_web/SpaguettiWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.sumativa.spaguetti_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaguettiWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
